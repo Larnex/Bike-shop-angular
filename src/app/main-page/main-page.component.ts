f13c70daa842d3c2a5d3b0fc5371412eb69d340a
Bohdan Asauliuk <bohdan.asauliuk@gmail.com>
2020-09-11 13:17:54 +0200

    commit

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
-import { goods } from '../../assets/goods';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { Bike } from '../../assets/bikes';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-main-page',
@@ -8,8 +8,43 @@ import { goods } from '../../assets/goods';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
-  goods = goods;
-  constructor() {}
+  constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getBikes();
+  }
+
+  showDetails: boolean = false;
+
+  getBikes(): void {
+    this.dataService.getBikes().subscribe((bikes) => (this.bikes = bikes));
+  }
+
+  bikes: Bike[];
+}
+
+// Custom Pipe to limit characters for description
+@Pipe({
+  name: 'charLimit',
+})
+export class CharLimitPipe implements PipeTransform {
+  transform(value: string, limit: number = 35, trail: String = '...'): string {
+    let result = value || '';
+
+    if (value) {
+      const words = value.split(/\s+/);
+
+      if (words.length > Math.abs(limit)) {
+        if (limit < 0) {
+          limit *= -1;
+          result =
+            trail + words.slice(words.length - limit, words.length).join(' ');
+        } else {
+          result = words.slice(0, limit).join(' ') + trail;
+        }
+      }
+    }
+
+    return result;
+  }
 }
