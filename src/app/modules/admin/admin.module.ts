81016dac402ef5d5c61015820af712f3d355fb61
Bohdan Asauliuk <bohdan.asauliuk@gmail.com>
2020-09-29 15:01:07 +0200

    add admin module and add new product component

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AddPageComponent } from './add-page/add-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: AdminLayoutComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: '/admin/login',
+            pathMatch: 'full',
+          },
+          {
+            path: 'login',
+            component: LoginPageComponent,
+          },
+          {
+            path: 'add',
+            component: AddPageComponent,
+          },
+        ],
+      },
+    ]),
+  ],
+  exports: [],
+  declarations: [AdminLayoutComponent, LoginPageComponent, AddPageComponent],
+})
+export class AdminModule {}
---
+++ src/app/modules/admin/login-page/login-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent implements OnInit {
+  form: FormGroup;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      login: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+}
